2e54cf09a4f55319fd54b44f5fad6ead707b4cbc
Sergio Vieri <sergio.vieri.hp@gmail.com>
2020-07-04 20:04:07 +0800

    Update run.sh

--- Discord/run.sh
+++ Discord/run.sh
@@ -1,1 +1,1 @@
-while :; do git fetch --all; git reset --hard origin/master; npm install all; node bot.js; done
+while :; do git fetch --all; git reset --hard origin/master; npm install; node bot.js; done
