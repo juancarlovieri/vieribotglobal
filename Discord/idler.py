0143e4286101177731731a68ced229f07aba88e9
juancarlovieri <juan.c.vieri@gmail.com>
2020-09-23 12:12:43 +0700

    change idler to online

--- Discord/idler.py
+++ Discord/idler.py
@@ -25,7 +25,7 @@ async def stop(ctx):
 @bot.event
 async def on_ready():
   print('ready')
-  await bot.change_presence(status=discord.Status.do_not_disturb)
-  
+  await bot.change_presence(status=discord.Status.online)
+
 bot.run(token, bot = False)
 
