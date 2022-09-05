a3769a268727eaa2e6f93302de80d517d042ce37
juancarlovieri <53759730+juancarlovieri@users.noreply.github.com>
2022-09-05 22:24:31 +0700

    Add google task feature (#26)
    * initial commit
    
    * fix epoch time
    
    * Change uncompleted to incomplete and fix reminders that expire too long.
    
    * Change uncompleted to incomplete in googlegle task api
    
    * Fix runAtDate diff Math.max position
    
    * use math round to fix epoch to second converter

---
+++ Discord/googleTask.js
@@ -0,0 +1,159 @@
+const Discord = require(`discord.js`);
+const fs = require('fs');
+const path = require('path');
+const process = require('process');
+const { authenticate } = require('@google-cloud/local-auth');
+const { google } = require('googleapis');
+const { Channel } = require('./googleTask/channel');
+const { logger } = require('./logger');
+const cmdName = 'tasks';
+
+const SCOPES = ['https://www.googleapis.com/auth/tasks.readonly'];
+const TOKEN_PATH = path.join(process.cwd(), 'datas/token.json');
+const CREDENTIALS_PATH = path.join(process.cwd(), 'auth.json');
+const api = require('./googleTask/api.js');
+
+const refreshInterval = 3600000;
+const remindTime = 86400000;
+const remindTimeStr = '1 day';
+
+function hasAdmin(msg) {
+  return msg.member.permissions.has(Discord.Permissions.FLAGS.MANAGE_CHANNELS);
+}
+
+function splitMsg(msg) {
+  return msg.content.split(/\s+/);
+}
+
+function logAndThrow(jobs, message) {
+  const errors = jobs
+    .filter((j) => j.status !== 'fulfilled')
+    .map((j) => j.reason);
+  errors.forEach((error) =>
+    logger.error(`${message} ${error.message}`, { error })
+  );
+  if (errors.length) {
+    throw new Error(message);
+  }
+  return jobs.filter((j) => j.status === 'fulfilled').map((j) => j.value);
+}
+
+async function sendMsg(bot, embed, channel) {
+  await bot.channels.cache.get(channel).send({ embeds: [embed] });
+}
+
+async function sendReminder({ bot, task, epoch }) {
+  const { title, notes } = task;
+  const embed = {
+    color: '#a832a4',
+    title: `${title} in ${remindTimeStr}`,
+    link: `https://tasksboard.com/app`,
+    description: notes,
+    fields: [{ name: `\u200B`, value: `<t:${Math.round(epoch / 1000)}>` }],
+    timestamp: new Date(),
+    footer: {
+      text: 'By Vieri Corp.™ All Rights Reserved',
+    },
+  };
+
+  var channels = await Channel.find().exec();
+  channels.map((c) => sendMsg(bot, embed, c.channelId));
+}
+
+function runAtDate(date, func, payload) {
+  var diff = date - Date.now();
+  if (diff < -10000) return;
+  diff = Math.max(diff, 0);
+  if (diff > 0x7fffffff)
+    setTimeout(() => {
+      runAtDate(date, func, payload);
+    }, 0x7fffffff);
+  else setTimeout(func, diff, payload);
+}
+
+var vis = [];
+
+async function refreshTask(bot, task) {
+  if (!task.due) return;
+
+  var epoch = new Date(task.due).getTime();
+  runAtDate(epoch - remindTime, sendReminder, { bot, task, epoch });
+
+  vis.push(task);
+}
+
+async function refreshTaskList(bot, taskList) {
+  const tasks = await api.getIncompleteTasks(taskList.id);
+  tasks.filter((t) => t.due != null);
+  const jobs = await Promise.allSettled(tasks.map((m) => refreshTask(bot, m)));
+  const results = logAndThrow(jobs, `Refresh task lists failed`);
+}
+
+async function refresh(bot) {
+  const taskLists = await api.getTaskLists();
+  const jobs = await Promise.allSettled(
+    taskLists.map((m) => refreshTaskList(bot, m))
+  );
+  const results = logAndThrow(jobs, `Tasks refresh failed.`);
+}
+
+async function init(bot) {
+  refresh(bot);
+  setInterval(() => refresh(bot), refreshInterval);
+}
+
+api.startLoadAuth();
+
+async function remind(bot, msg) {
+  if (!hasAdmin(msg)) {
+    msg.channel.send(`no`);
+    return;
+  }
+
+  const channelId = msg.channel.id;
+  const existingChannel = await Channel.findOne({ channelId }).exec();
+
+  if (existingChannel) {
+    msg.channel.send(`already bruh`);
+    return;
+  }
+
+  const newChannel = new Channel({ channelId });
+  await newChannel.save();
+  msg.channel.send(`Saved!`);
+  logger.info(`Saved new channel ${channelId}`);
+}
+
+async function token(bot, msg) {
+  const args = msg.content.split(/\s+/);
+  if (args.length !== 3) {
+    msg.channel.send(`wot`);
+    return;
+  }
+
+  const res = await api.saveRefreshToken(args[2]);
+  if (res.success) msg.channel.send(`saved!`);
+  else msg.channel.send(`failed!`);
+}
+
+const cmdMap = {
+  remind,
+  token,
+};
+
+async function cmd(bot, msg) {
+  try {
+    const args = msg.content.split(/\s+/);
+    if (args.length === 1) return;
+    if (!(args[1] in cmdMap)) return;
+    await cmdMap[args[1]](bot, msg);
+  } catch (error) {
+    logger.error(`${cmdName} error: ${error.message}`, { error });
+    msg.channel.send(`Unknown error occured: ${error.message}`);
+  }
+}
+
+module.exports = {
+  cmd,
+  init,
+};
---
+++ Discord/googleTask/api.js
@@ -0,0 +1,73 @@
+const fs = require('fs');
+const path = require('path');
+const process = require('process');
+const { authenticate } = require('@google-cloud/local-auth');
+const { google } = require('googleapis');
+const { logger } = require('../logger');
+
+const SCOPES = ['https://www.googleapis.com/auth/tasks.readonly'];
+const TOKEN_PATH = path.join(process.cwd(), 'datas/token.json');
+const CREDENTIALS_PATH = path.join(process.cwd(), 'auth.json');
+const auth = require('../auth.json');
+var service;
+
+async function getRefreshToken() {
+  try {
+    var content = await fs.readFileSync(TOKEN_PATH);
+    var credentials = JSON.parse(content);
+    if (credentials.refresh_token) return credentials.refresh_token;
+    return null;
+  } catch (err) {
+    logger.error(`Failed to load token_path`, { err });
+    return;
+  }
+}
+
+async function saveRefreshToken(refresh_token) {
+  try {
+    await fs.writeFileSync(TOKEN_PATH, JSON.stringify({ refresh_token }));
+    logger.info(`Saved new refresh_token.`);
+    return { success: true };
+  } catch (err) {
+    logger.error(`Failed to load token_path`, { err });
+    return { success: false };
+  }
+}
+
+async function startLoadAuth() {
+  const { client_secret, client_id, redirect_uris } = auth.googleApi;
+  const oAuth2Client = new google.auth.OAuth2(
+    client_id,
+    client_secret,
+    redirect_uris[0]
+  );
+
+  const refresh_token = await getRefreshToken();
+
+  if (!refresh_token) return;
+
+  var newToken = await oAuth2Client.refreshToken(refresh_token);
+  newToken = newToken.tokens;
+  oAuth2Client.setCredentials(newToken);
+  service = google.tasks({ version: 'v1', auth: oAuth2Client });
+}
+
+async function getTaskLists() {
+  const res = await service.tasklists.list({ maxResults: 100 });
+  return res.data.items;
+}
+
+async function getIncompleteTasks(taskId) {
+  const res = await service.tasks.list({
+    tasklist: taskId,
+    showCompleted: false,
+  });
+  return res.data.items;
+}
+
+module.exports = {
+  startLoadAuth,
+  getTaskLists,
+  getIncompleteTasks,
+  saveRefreshToken,
+};
