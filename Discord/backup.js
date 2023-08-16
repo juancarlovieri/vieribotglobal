c6b9c08c0238c3241167acb61db3a01864dd629c
Juan Carlo Vieri <juan.c.vieri@gmail.com>
2023-08-16 15:28:45 +0800

    Fix zipping datas.zip again.

--- Discord/backup.js
+++ Discord/backup.js
@@ -9,8 +9,8 @@ const { logger } = require('./logger');
 async function send() {
   logger.info(`Backup started.`);
   try {
-    if (fs.existsSync(`./datas/datas.zip`))
-      await fs.unlinkSync(`./datas/datas.zip`);
+    // if (fs.existsSync(`./datas/datas.zip`))
+    //   await fs.unlinkSync(`./datas/datas.zip`);
     var output = fs.createWriteStream(`datas/datas.zip`);
     var archive = archiver(`zip`);
 
@@ -29,7 +29,8 @@ async function send() {
 
     archive.pipe(output);
 
-    archive.directory(`datas/`, `datas`);
+    // archive.directory(`datas/`, `datas`);
+    archive.glob(`**/*`, { cwd: `datas`, ignore: [`datas.zip`] }, {});
 
     logger.info(`Zipping started`);
     archive.finalize();
